fe3533bb3b1a0d356ba785452a44902454f0c0a6
juaniigauna <nachogauna83@gmail.com>
2020-10-17 17:06:54 -0300

    Bug fixed

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,22 +1,9 @@
 let router = require('express').Router()
 
 router.get('/', (req, res) => {
-    con.query(
-        'SELECT * FROM words',
-        (err, rows) => {
-            if (err) {
-                res.render('index', {
-                    path: 'errors/errors.ejs',
-                    error: err 
-                })
-            } else {
-                res.render('index', {
-                    path: 'dashboard/index.ejs',
-                    array: rows
-                })
-            }
-        }
-    )
+    res.render('index', {
+        path: 'dashboard/index.ejs',
+    })
 })
 router.get('/settings', (req, res) => {
     res.render('index', {
